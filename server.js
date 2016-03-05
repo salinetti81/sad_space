65aed9dd090812dd2d932f7b06d25e45093a97cf
aadams22 <ashleighadams@Ashleighs-MacBook-Air.local>
2016-03-05 16:11:45 -0500

    committing server page setup

---
+++ server.js
@@ -0,0 +1,30 @@
+var express 	 	= require('express'),
+		bodyParser 	= require('body-parser'),
+		mongoose		= require('mongoose'),
+		morgan 			= require('morgan'),
+		passport 		= require('passport'),
+		session 		= require('express-session'),
+		port 				= process.env.PORT || 3000,
+		app					= express();
+
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json);
+
+app.use(morgan('dev'));
+
+app.use(session({secret: 'secretsecretsecret', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport.js')(passport);
+
+app.get('/', function(req,res){
+	res.render('index.html');
+});
+
+app.listen(port, function(){
+	console.log('Today will be a good day');
+});
