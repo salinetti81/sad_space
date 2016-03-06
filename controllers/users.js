a8b9270902530633197c2a11db686fd6fb39a89e
Daniel Salinetti <dansalinetti@gmail.com>
2016-03-06 16:29:41 -0500

    Commiting updated passport stuff

--- controllers/users.js
+++ controllers/users.js
@@ -1,33 +1,38 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
+var express 	= require('express'),
+		router 		= express.Router(),
+		passport 	= require('passport'),
+		User 			= require('../models/userModel');
 
-var User = require('../models/userModel');
 
 //Index
-// router.get('/', function(req, res) {
-//   res.locals.login = req.isAuthenticated();
-//   User.find(function(err, data) {
-//       res.render('./signup.html', {users: data});
-//   });
-// });
+router.get('/', function(req,res) {
+ 	res.render('index.html');
+});
+
+router.get('/:id', function(req,res){
+	res.locals.login = req.isAuthenticated();
+	 User.find(function(err, data) {
+      res.render('index.html', {users: data});
+      console.log(err);
+  });
+});
 
 
 // CREATE NEW USER
     //PROCESS SIGNUP FORM
-router.post('/', passport.authenticate('local-signup', {
-    failureRedirect : '/users'}), function(req, res) { //redirect back to signup if there is an error
-        res.redirect('/users/' + req.user.id);
+router.post('/singup', passport.authenticate('local-signup', {
+    failureRedirect : '/'}), function(req,res) { //redirect back to signup if there is an error
+        res.redirect('/' + req.user.id);
         console.log(users);
 });
+
+
     //PROCESS THE LOGIN FORM
 router.post('/login', passport.authenticate('local-login', {
     // successRedirect : '/profile', // redirect to the secure profile section
-   failureRedirect : '/users'}), function(req, res) { // redirect back to the signup page if there is an error
-        res.redirect('/users/' + req.user.id);
+   failureRedirect : '/'}), function(req,res) { // redirect back to the signup page if there is an error
+        res.redirect('/' + req.user.id);
 });
-
-
 
 
 //defines isLoggedIn
@@ -38,6 +43,5 @@ function isLoggedIn(req, res, next) {
 //if user doesn't exists, go here
       res.redirect('/');
 } ;
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var express 	 	= require('express'),
 		port 				= process.env.PORT || 3000,
 		app					= express();
 
+mongoose.connect('mongodb://localhost:27017/planetproject');
+
 
 app.use(express.static('public'));
 
@@ -20,11 +22,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 var usersController = require('./controllers/users');
+app.use('/milkyway', usersController);
+
 require('./config/passport.js')(passport);
 
-app.get('/', function(req,res){
-	res.render('signin.html');
-});
+
+// app.get('/milkyway', function(req,res){
+// 	res.render('index.html');
+// });
 
 app.listen(port, function(){
 	console.log('Today will be a good day');
