508bc62092df857585fe944d45e27668142ece4e
aadams22 <ashleighadams@Ashleighs-MacBook-Air.local>
2016-03-06 13:46:05 -0500

    committing

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('SadSpace', []);
+
+
+$.ajax( {
+    url: remoteUrlWithOrigin,
+    data: queryData,
+    dataType: 'json',
+    type: 'POST',
+    headers: { 'Api-User-Agent': 'Example/1.0' },
+    success: function(data) {
+       // do something with data
+    }
+} );
