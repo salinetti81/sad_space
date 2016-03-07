26204c26c9b9e16c320516cb44c31516a98f91c7
aadams22 <ashleighadams@Ashleighs-MacBook-Air.local>
2016-03-07 12:39:58 -0500

    committign PlanetController

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,41 @@
-
-var app = angular.module('SadSpace', []);
-
-app.controller('MainController', function(){
-	
-});
-
 var app = angular.module('sadSpace', []);
 
 
-app.controller('MainController', [function(){
+// app.controller('MainController', [function(){
 
 	
-}]);
+// }]);
 
+app.controller('PlanetController', ['$http','$scope', function($http,$scope){
+	var controller = this;
+
+	$scope.getExtract = function(event){
+		console.log(this);
+		console.log(event.target.id);
+		console.log('function accessed');
+			// $.http({
+
+	  //    url: "https://en.wikipedia.org/w/api.php?action=query&format=json&indexpageids&prop=revisions&titles=mars&prop=extracts&exintro",
+	  //    method: "GET",
+	  //    dataType: 'jsonp'
+	  // 	}).then(
+	  // 	function(result){
+	  // 		console.log(result);
+	  // 		// getExtract(result);
+	  // 		var pageID = result.query.pageids;
+	  // 		var number = parseInt(pageID);
+	  // 		var extract = result.query.pages[number].extract;
+   //    	console.log(extract);
+   //    	// $('#info').html(extract);
+
+	  // 	}),
+	  // 	function(err){
+	  // 		console.log(err);
+	  // 	}
+	  }; //<--getExtract
+
+
+
+
+
+}]);
