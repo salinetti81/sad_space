d1ea5f07e881fcf053f5786a9fee83cd1fc8d611
Daniel Salinetti <dansalinetti@gmail.com>
2016-03-06 19:22:40 -0500

    Fixing merge iss issue

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
  var passport = require('passport');
-=======
-var passport = require('passport');
->>>>>>> passport
 
 var LocalStrategy  = require('passport-local').Strategy;
 
@@ -12,15 +8,7 @@ var User = require('../models/userModel.js');
 // expose this function to our app using module.exports
 module.exports = function(passport) {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
 // =========================================================================
->>>>>>> passport
-=======
-// =========================================================================
->>>>>>> 685542ef5324775d1fd8d28af5cc614082a3139f
 // passport session setup ==================================================
 // =========================================================================
 // required for persistent login sessions
@@ -73,7 +61,7 @@ User.findOne({ 'email' :  email }, function(err, user) {
         // set the user's local credentials
         newUser.username = req.body.username;
         // newUser.email    = email;
-        // This is breaking the server//////
+        // ///// Needed to comment out this line for passport to not kill the server//////
         // newUser.password = newUser.generateHash(password);
 
         // save the user
@@ -81,30 +69,6 @@ User.findOne({ 'email' :  email }, function(err, user) {
             if (err)
                 throw err;
             return done(null, newUser);
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
-
-    // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
-    done(null, user.id);
-    });
-
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            done(err, user);
-
-=======
->>>>>>> passport
-=======
->>>>>>> 685542ef5324775d1fd8d28af5cc614082a3139f
         });
     };
 
@@ -131,7 +95,6 @@ User.findOne({ 'email' :  email }, function(err, user) {
     // if there are any errors, return the error before anything else
     if (err)
         return done(err);
-<<<<<<< HEAD
 
     // if no user is found, return the message
     if (!user)
@@ -145,220 +108,8 @@ User.findOne({ 'email' :  email }, function(err, user) {
     return done(null, user);
 
 
-});
-
-}));
-
-<<<<<<< HEAD
-}; //ends module.exprts
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var passport       = require('passport'),
-//     LocalStrategy  = require('passport-local').Strategy;
-
-// // load up the user model
-// var User = require('../models/userModel.js');
-
-// // expose this function to our app using module.exports
-// module.exports = function(passport) {
-
-// // =========================================================================
-// // passport session setup ==================================================
-// // =========================================================================
-// // required for persistent login sessions
-// // passport needs ability to serialize and unserialize users out of session
-
-// // used to serialize the user for the session
-// passport.serializeUser(function(user, done) {
-// done(null, user.id);
-// });
-
-// // used to deserialize the user
-// passport.deserializeUser(function(id, done) {
-// User.findById(id, function(err, user) {
-//     done(err, user);
-// });
-// });
-
-// // =========================================================================
-// // LOCAL SIGNUP ============================================================
-// // =========================================================================
-
-// passport.use('local-signup', new LocalStrategy({
-// // by default, local strategy uses username and password, we will override with email
-// usernameField : 'email',
-// passwordField : 'password',
-// passReqToCallback : true // allows us to pass back the entire request to the callback
-// },
-// function(req, email, password, done) {
-
-// // asynchronous
-// // User.findOne wont fire unless data is sent back
-// process.nextTick(function() {
-
-// // find a user whose email is the same as the forms email
-// // we are checking to see if the user trying to login already exists
-// User.findOne({ 'email' :  email }, function(err, user) {
-//     // if there are any errors, return the error
-//     if (err)
-//         return done(err);
-
-//     // check to see if theres already a user with that email
-//     if (user) {
-//         return done(null, false);
-//     } else {
-
-//         // if there is no user with that email
-//         // create the user
-//         var newUser = new User();
-
-//         // set the user's local credentials
-//         newUser.username = req.body.username;
-//         // newUser.email    = email;
-//         newUser.password = newUser.generateHash(password);
-
-//         // save the user
-//         newUser.save(function(err) {    
-//             if (err)
-//                 throw err;
-//             return done(null, newUser);
-//     // =========================================================================
-//     // passport session setup ==================================================
-//     // =========================================================================
-//     // required for persistent login sessions
-//     // passport needs ability to serialize and unserialize users out of session
-
-//     // used to serialize the user for the session
-//     passport.serializeUser(function(user, done) {
-//     done(null, user.id);
-//     });
-
-//     // used to deserialize the user
-//     passport.deserializeUser(function(id, done) {
-//         User.findById(id, function(err, user) {
-//             done(err, user);
-
-//         });
-//     });
-
-//     // =========================================================================
-//     // LOCAL SIGNUP ============================================================
-//     // =========================================================================
-
-//     passport.use('local-signup', new LocalStrategy({
-//     // by default, local strategy uses username and password, we will override with email
-//     usernameField : 'email',
-//     passwordField : 'password',
-//     passReqToCallback : true // allows us to pass back the entire request to the callback
-//     },
-//     function(req, email, password, done) {
-
-//     // asynchronous
-//     // User.findOne wont fire unless data is sent back
-//     process.nextTick(function() {
-
-//         // find a user whose email is the same as the forms email
-//         // we are checking to see if the user trying to login already exists
-//         User.findOne({ 'email' :  email }, function(err, user) {
-//             // if there are any errors, return the error
-//             if (err)
-//                 return done(err);
-
-//             // check to see if theres already a user with that email
-//             if (user) {
-//                 return done(null, false);
-//             } else {
-
-//                 // if there is no user with that email
-//                 // create the user
-//                 var newUser = new User();
-
-//                 // set the user's local credentials
-//                 newUser.username = req.body.username;
-//                 newUser.email    = email;
-//                 newUser.password = newUser.generateHash(password);
-
-//                 // save the user
-//                 newUser.save(function(err) {
-//                     if (err)
-//                         throw err;
-//                     return done(null, newUser);
-//                 });
-//             };
-
-//         }); 
-           
-//     });
-//     }));
-
-//      // =========================================================================
-//     // LOCAL LOGIN =============================================================
-//     // =========================================================================
-
-//     passport.use('local-login', new LocalStrategy({
-//     // by default, local strategy uses username and password, we will override with email
-//     usernameField : 'email',
-//     passwordField : 'password',
-//     passReqToCallback : true // allows us to pass back the entire request to the callback
-//     },
-//     function(req, email, password, done) { // callback with email and password from our form
-
-//     // find a user whose email is the same as the forms email
-//     // we are checking to see if the user trying to login already exists
-//         User.findOne({ 'email' :  email }, function(err, user) {
-//             // if there are any errors, return the error before anything else
-//             if (err)
-//                 return done(err);
-
-//             // if no user is found, return the message
-//             if (!user)
-//                 return done(null, false); // req.flash is the way to set flashdata using connect-flash
-
-//             // if the user is found but the password is wrong
-//             if (!user.validPassword(password))
-//                 return done(null, false); // create the loginMessage and save it to session as flashdata
-=======
-
-    // if no user is found, return the message
-    if (!user)
-        return done(null, false); // req.flash is the way to set flashdata using connect-flash
-
-    // if the user is found but the password is wrong
-    if (!user.validPassword(password))
-        return done(null, false); // create the loginMessage and save it to session as flashdata
-
-    // all is well, return successful user
-    return done(null, user);
->>>>>>> passport
-
-//             // all is well, return successful user
-//             return done(null, user);
-
-<<<<<<< HEAD
-
-//         });
-
-//     }));
-
-// }; //ends module.exprts
-=======
 });
 
 }));
 
 }; //ends module.exports
->>>>>>> passport
-=======
-}; //ends module.exports 
->>>>>>> 685542ef5324775d1fd8d28af5cc614082a3139f
--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 var app = angular.module('SadSpace', []);
 
 app.controller('MainController', function(){
 	
 });
-=======
+
 var app = angular.module('sadSpace', []);
 
 
@@ -12,4 +12,4 @@ app.controller('MainController', [function(){
 
 	
 }]);
->>>>>>> 685542ef5324775d1fd8d28af5cc614082a3139f
+
