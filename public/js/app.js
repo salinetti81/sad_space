5a4ad894f70349d255139834669664d2a2b17d43
Daniel Salinetti <dansalinetti@gmail.com>
2016-03-07 16:23:06 -0500

    Updating passport

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,14 @@ var app = angular.module('sadSpace', []);
 app.controller('SignUpController', ['$http', function($http){
     var controller = this;
     this.signUpData = {};
+    this.isLoggedIn = false;
 
     this.signUp = function(){
         console.log("SIGNUP function firing in app.js");
 
         console.log(this.signUpData);
 
+        if (!user)
         $http({
             method: 'POST',
             url: '/users/signup',
@@ -19,6 +21,7 @@ app.controller('SignUpController', ['$http', function($http){
             //success
             function(response){
                 console.log(response);
+                controller.isLoggedIn = true;
                 controller.signUpData = {};
             },
             function(err){
@@ -26,7 +29,7 @@ app.controller('SignUpController', ['$http', function($http){
             });       
 
     }; //<--signUp
-
+    if (user)
     this.logIn = function(){
         console.log("LOGIN function firing in app.js")
 
