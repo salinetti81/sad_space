d9287b54813085df67eaa255d469ffda0fa5643f
Daniel Salinetti <dansalinetti@gmail.com>
2016-03-09 15:51:00 -0500

    Trying to get neptune game into angular

--- public/js/gameapp.js
+++ public/js/gameapp.js
@@ -45,9 +45,81 @@ app.controller('UranusController', ['$http', '$scope', function($http, $scope){
 	
 }])
 
+//neptune start 
 app.controller('NeptuneController', ['$http', '$scope', function($http, $scope){ 
 	var controller = this
 	this.planet = 'Neptune';
+console.log("Don't Panic.")
+
+var memory_array = [ 'Thalassa', 'Thalassa', 'Despina', 'Despina', 'Galatea', 'Galatea', 'Naiad', 'Naiad', 'Larissa', 'Larissa', 'Proteus', 'Proteus', 'Triton', 'Triton', 'Nereclass', 'Nereclass', 'Halimede', 'Halimede' ];
+var memory_values = [];
+var memory_tile_ids = [];
+var tiles_flipped = 0;
+
+
+//shuffle tiles for new game
+Array.prototype.memory_tile_shuffle = function() {
+	var i = this.length, j, temp;
+	while(--i > 0) {
+		j = Math.floor(Math.random() * (i+1));
+		temp = this[j];
+		this[j] = this[i];
+		this[i] = temp;
+	}
+}
+
+//create a new board
+function newBoard(){
+	tiles_flipped = 0;
+	var output = '';
+    memory_array.memory_tile_shuffle();
+	for(var i = 0; i < memory_array.length; i++){
+		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+	}
+	angular.div('memory_board').innerHTML = output;
+}
+  window.addEventListener(newBoard());
+
+function memoryFlipTile(tile,val){
+	if(tile.innerHTML == "" && memory_values.length < 2){
+		tile.style.background = '#FFF';
+		tile.innerHTML = val;
+		if(memory_values.length == 0){
+			memory_values.push(val);
+			memory_tile_ids.push(tile.id);
+		} else if(memory_values.length == 1){
+			memory_values.push(val);
+			memory_tile_ids.push(tile.id);
+			if(memory_values[0] == memory_values[1]){
+				tiles_flipped += 2;
+				// Clear both arrays
+				memory_values = [];
+            	memory_tile_ids = [];
+				// Check to see if the whole board is cleared
+				if(tiles_flipped == memory_array.length){
+					alert("You won!");
+					document.getElementById('memory_board').innerHTML = "";
+					newBoard();
+				}
+			} else {
+				function flip2Back(){
+				    // Flip the 2 tiles back over
+				    var tile_1 = document.getElementById(memory_tile_ids[0]);
+				    var tile_2 = document.getElementById(memory_tile_ids[1]);
+				    tile_1.style.background = 'blue';
+            	    tile_1.innerHTML = "";
+				    tile_2.style.background = 'blue';
+            	    tile_2.innerHTML = "";
+				    // Clear both arrays
+				    memory_values = [];
+            	    memory_tile_ids = [];
+				}
+				setTimeout(flip2Back, 500);
+			}
+		}
+	}
+}// ends neptune  
+
 	
 }])
 
@@ -56,6 +128,8 @@ app.controller('PlutoController', ['$http', '$scope', function($http, $scope){
 	this.planet = 'Pluto';
 	
 }])
+
+
 
 
 
